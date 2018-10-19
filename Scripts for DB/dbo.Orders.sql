2f8e8ea7d2b54b3029a220abfa39f7b36116c178
OrProjectsEylon <OrProjectsEylon@gmail.com>
2018-10-19 19:34:34 +0300

    Scripts for the DB

---
+++ Scripts for DB/dbo.Orders.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderID]       INT      NOT NULL,
+    [UserID]        INT      NOT NULL,
+    [OrderDate]     DATETIME NOT NULL,
+    [ItemID]        INT      NOT NULL,
+    [PickUpPointID] INT      NOT NULL,
+    [Size]          INT      NOT NULL,
+    [Quantity]      INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([OrderID] ASC),
+    FOREIGN KEY ([ItemID]) REFERENCES [dbo].[Shoes] ([ItemID]),
+    FOREIGN KEY ([PickUpPointID]) REFERENCES [dbo].[PickUpPoints] ([ID]),
+    FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID]),
+    FOREIGN KEY ([ItemID]) REFERENCES [dbo].[Shirts] ([ItemID]),
+    FOREIGN KEY ([ItemID]) REFERENCES [dbo].[Trousers] ([ItemID])
+);
+
---
+++ Scripts for DB/dbo.Trousers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Trousers] (
+    [ItemID]  INT        NOT NULL,
+    [Price]   FLOAT (53) NOT NULL,
+    [Color]   NCHAR (10) NOT NULL,
+    [Size]    INT        NOT NULL,
+    [Brand]   NCHAR (10) NOT NULL,
+    [Gender]  NCHAR (10) NOT NULL,
+    [ImgPath] NCHAR (40) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ItemID] ASC)
+);
+
