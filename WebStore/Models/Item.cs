e6f2a0098d1e1e22daac3ace4e35626c0970b817
unknown <annush110396@gmail.com>
2018-11-03 14:13:08 +0200

    add order not working :(

--- WebStore/Models/Item.cs
+++ WebStore/Models/Item.cs
@@ -16,6 +16,20 @@ namespace WebStore.Models
         public string Gender { get; set; }
         public string ImgPath { get; set; }
         public bool IsDeleted { get; set; }
+
+        public Item()
+        {
+        }
+
+        public Item(int itemTypeId, double price, string color, string brand, string gender, string imgPath)
+        {
+            ItemTypeId = itemTypeId;
+            Price = price;
+            Color = color;
+            Brand = brand;
+            Gender = gender;
+            ImgPath = imgPath;
+        }
     }
 }
 
