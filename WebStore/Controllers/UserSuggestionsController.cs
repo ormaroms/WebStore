89a948304697009501a0da17d85b9dbf5037ec09
gilurman <gilurman31@gmail.com>
2018-11-03 13:42:54 +0200

    fixed graph

---
+++ WebStore/Controllers/UserSuggestionsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class UserSuggestionsController : Controller
+    {
+        // GET: UserSuggestions
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult PredictUserSuggestions()
+        {
+            //int userId =n
+            return null;
+        }
+    }
+}
